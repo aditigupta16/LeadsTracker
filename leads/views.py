da148598508d290ba868a1c3be7d28b7c2f96cb9
aditi-thistle <aditi@thistle.co>
2020-09-12 21:41:21 +0530

    initial commit

---
+++ leads/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from .models import Lead
+from .serializers import LeadSerializer
+from rest_framework import generics
+
+
+# Create your views here.
+
+
+class LeadListCreateAPIView(generics.ListCreateAPIView):
+
+	queryset = Lead.objects.all()
+	serializer_class = LeadSerializer
